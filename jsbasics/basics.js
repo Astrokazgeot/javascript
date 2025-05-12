ef7a79b2d9249b638e47b9d0371e2f89fc86afb1
Astrokazgeot <116888017+Astrokazgeot@users.noreply.github.com>
2025-05-12 13:03:41 +0000

    basic datatypes

---
+++ jsbasics/basics.js
@@ -0,0 +1,13 @@
+const accountId=27
+let accountEmail="some@gmail.com"
+accountCity="Delhi"  // bad practice declare as let or const
+//accountId=3 not allowed 
+let state//give undefined if print
+accountCity="Bengal"
+accountEmail="why@gmail.com"
+
+
+console.table([accountId,accountEmail,accountCity])
+console.log(typeof accountEmail)
+console.log(typeof null) // answer its type is object
+console.log(typeof undefined)  // answer its type is undefined only
