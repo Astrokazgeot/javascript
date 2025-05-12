025f64f18e5e69447a6f6e7ba4e398ea69fad05f
Astrokazgeot <116888017+Astrokazgeot@users.noreply.github.com>
2025-05-12 18:14:23 +0000

    conversion

---
+++ jsbasics/convdatatypes.js
@@ -0,0 +1,17 @@
+let score=undefined
+console.log(typeof score)
+let valInNumber=Number(score)// N capital
+console.log(typeof valInNumber)
+console.log(valInNumber) // 33abc show NaN not a number but we converted it to number
+let sike="hello"
+let sik=Number(sike)
+console.log(typeof sik)
+console.log(sik)
+
+// 1 conversion to boolwan give true and 0 false
+// "" conversion to boolean give false if string like "hello" it give true 
+ let num=21
+ let numToString=String(num)
+ console.log(typeof numToString)
+console.log(numToString)  // converted to string
+
