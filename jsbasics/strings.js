e6526bf8520b2d2d4faee6e893b1e9ba60789a2f
Astrokazgeot <116888017+Astrokazgeot@users.noreply.github.com>
2025-06-07 09:32:52 +0000

    basic string function

--- jsbasics/strings.js
+++ jsbasics/strings.js
@@ -1,15 +1,19 @@
-const name="kazgeot"
-const id=23921
-const gameName= new String('Manchester United')// another way of declaring creates a string object
-console.log(typeof gameName);// typeof is object coz new creates a string object that contains 
-// manchester united and string protocol
-console.log(id);
-console.log(name)// type of is string
-console.log(gameName[0])
-console.log(name.__proto__)// writing proto create a string object of type string and it 
-//point to String.prototype that contain various operations like uppercase
-// it doesnt directly display methods of prototype thats why it appears empty the object
+const name="Messi"
+const count= 50
+console.log(typeof(name))
+console.log(`Hello my name ${name} and count is ${count}`) // string interpolation used back ticks
 
-console.log(gameName.toUpperCase());// will not change original coz nothing changed in object that 
-// is in heap it created a refrence in stack that points to object in heap that have uppercase
 
+const gameName= new String('Neymar')
+console.log(gameName[0]);
+console.log(name[0])
+
+//console.log(gameName.__proto__)
+//console.log(gameName.toUpperCase())
+//console.log(name.toUpperCase())// JS temporary create object and does operation and destroy it object
+console.log(gameName.slice(-3,1)) // start is behind end so it dont print
+console.log(gameName.slice(-5,3))
+
+
+const strin2="   what "
+console.log(strin2.trim())
